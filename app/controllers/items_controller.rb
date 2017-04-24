4af8924b1f79e1fff5d3e2d45f36449663d4c085
gotooza <510hidepon@gmail.com>
2017-04-24 01:26:05 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,12 +13,17 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+end
 
   private
 
@@ -35,4 +40,3 @@ class ItemsController < ApplicationController
       image_url: image_url,
     }
   end
-end
